77324ddd04abd3cc563f82385d3594dd76bdf430
StephGarage404 <stephane.1995@hotmail.fr>
2024-01-09 17:01:01 +0100

    mini

---
+++ htdocs/index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AutoForum</title>
+    <link rel="stylesheet" href="./style/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    
+</head>
+<body>
+    
+
+    <header>
+
+        <nav class="navbar navbar-expand-lg d-flex align-items-right">
+            
+            <h1 class="titre">MINI CHAT</h1>
+            <div class="container d-flex justify-content-end">
+                
+
+
+                <a class="navbar-brand text-white" href="#">Se connecter</a>
+                    
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+            
+
+                <a class="navbar-brand text-white" href="#">S'inscrire</a>
+                    
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+    
+             </div>
+
+        </nav>
+
+    </header>
+
+
+
+    <section class="Card p-3 text-white ">
+
+            <!-- DEBUT CODE HAUT DE PAGE!-->
+
+            <div class="title row">
+
+                <div class="col-6 fs-3 ">
+                    <p >
+                    Bienvenue aux passionnés automobiles! Rejoignez notre forum pour partager vos expériences et discuter d'événements auto passionnants.
+                    </p>
+                </div>
+
+                <div class="col-3">
+
+                </div>
+
+                <div class="peopletalking col col-3">
+                    <img src="./images/pngegg(2).png">
+                </div>
+
+            </div>
+
+            <!-- FIN CODE HAUT DE PAGE!-->
+
+
+
+        
+
+        <div class="row mt-5">
+
+            <!-- DEBUT CODE PARTIE DROITE!-->
+
+            <div class="col col-6">
+
+                <div class="tchat row mx-5">
+
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex saepe nam doloremque cupiditate nesciunt odio laborum nostrum tempore vero aliquid corrupti cum inventore, fugiat, ea voluptate atque dolor? Quo, ratione.
+                </div>
+
+                <div class="row mclaren">
+                        <img src="./images/pngegg(6).png" alt="mclaren rouge et noir">
+                </div>
+
+            </div>
+
+            <!-- FIN CODE PARTIE DROITE!-->
+
+
+            <!-- DEBUT CODE PARTIE MILIEU!-->
+
+           
+                <div class="col col-2 barredemerde ps-5">
+                    <div class="vr" style="height: 450px; width: 3px;"></div>        
+                </div>
+
+          
+
+            <!-- FIN CODE PARTIE MILIEU!-->
+                
+
+            <!-- DEBUT CODE PARTIE GAUCHE!-->
+
+            <div class="col col-4">
+                <div class="user">
+                   <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita necessitatibus aut distinctio iusto rem repellendus. Quibusdam nesciunt illo aperiam id alias fugit voluptates, error vel explicabo, hic veniam dolores tempore.</p>
+                </div>
+            </div>
+            <!-- FIN CODE PARTIE GAUCHE!-->
+
+        </div>
+
+      
+
+    </section>
+
+
+
+
+
+
+
+
+
+
+    <footer>
+
+        
+    </footer>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
